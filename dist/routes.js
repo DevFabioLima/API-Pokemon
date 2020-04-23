01f99d838c3205b2984f7bf7498f08b0e882b4dc
fabio lucas lima <fabiolucaslima@MacBook-Air-de-fabio.local>
2020-04-22 23:30:33 -0300

    Desafio finalizado e readme

--- dist/routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _express = require('express');
-
-var _WeatherController = require('./app/controllers/WeatherController'); var _WeatherController2 = _interopRequireDefault(_WeatherController);
-
-const routes = new (0, _express.Router)();
-
-routes.get('/buscar/:city', _WeatherController2.default.getPokemon);
-
-exports. default = routes;
